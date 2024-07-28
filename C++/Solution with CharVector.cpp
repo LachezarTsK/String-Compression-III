e8a06c5e54114a5fb9a7bc5df760643a480e9798
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-28 15:27:42 +0200

    Create Solution with CharVector.cpp

---
+++ C++/Solution with CharVector.cpp
@@ -0,0 +1,28 @@
+
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+
+public:
+    string compressedString(const string& word) const {
+        vector<char> compressed;
+        compressed.reserve(2 * word.size());
+        int frequency = 1;
+
+        for (int i = 1; i < word.size(); ++i) {
+            if (frequency == 9 || word[i - 1] != word[i]) {
+                compressed.push_back(frequency + '0');
+                compressed.push_back(word[i - 1]);
+                frequency = 1;
+                continue;
+            }
+            ++frequency;
+        }
+        compressed.push_back(frequency + '0');
+        compressed.push_back(word[word.size() - 1]);
+
+        return string(compressed.begin(), compressed.end());
+    }
+};
