d84eba693cae3f8eae96fe52b8b08020885dc83e
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-07-28 14:58:55 +0200

    Create Solution with StringBuilder.kt

---
+++ Kotlin/Solution with StringBuilder.kt
@@ -0,0 +1,22 @@
+
+class Solution {
+
+    fun compressedString(word: String): String {
+        val compressed = StringBuilder(2 * word.length)
+        var frequency = 1
+
+        for (i in 1..<word.length) {
+            if (frequency == 9 || word[i - 1] != word[i]) {
+                compressed.append(frequency)
+                compressed.append(word[i - 1])
+                frequency = 1
+                continue;
+            }
+            ++frequency;
+        }
+        compressed.append(frequency)
+        compressed.append(word[word.length - 1])
+
+        return compressed.toString()
+    }
+}
